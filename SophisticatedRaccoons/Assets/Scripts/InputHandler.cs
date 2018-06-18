582fa5de4ea9c76306d555c3e576dbcf6c48f0b3
Mitja <mitja.immonen@hotmail.com>
2018-06-18 15:05:19 +0300

    Gitignore + project with all basic assets

---
+++ SophisticatedRaccoons/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class InputHandler : MonoBehaviour {
+
+	// List<Player> players = new List<Player>();
+	GamepadStateHandler gamepad;
+	StateHandler stateHandler; 
+
+	void Awake()
+	{
+		gamepad = GetComponent<GamepadStateHandler>();
+		stateHandler = GetComponent<StateHandler>();
+	}
+	public PlayerGamepadData HandleInput(PlayerGamepadData gamepadData)
+	{
+		if (stateHandler.gamestate == GameState.menu)
+		{
+			gamepadData = HandleMenuInputs(gamepadData);
+		}
+		if (stateHandler.gamestate == GameState.game)
+		{
+			// if (gamepadData.characterIndex != -1)
+			// 	players[gamepadData.characterIndex].HandleInput(gamepadData.state, gamepadData.prevState);
+		}
+
+		return gamepadData;
+	}
+
+	public PlayerGamepadData HandleFixedInput(PlayerGamepadData gamepadData)
+	{
+		if (stateHandler.gamestate == GameState.game)
+		{
+			// if (gamepadData.characterIndex != -1)
+				// players[gamepadData.characterIndex].HandleFixedInput(gamepadData.state, gamepadData.prevState);
+		}
+		return gamepadData;
+	}
+	
+
+	PlayerGamepadData HandleMenuInputs(PlayerGamepadData gamepadData)
+	{
+		//Handle player (gamepad) activation - check if Y-button was pressed in this gamepad in this frame
+		if (gamepadData.prevState.Buttons.Y == ButtonState.Released && gamepadData.state.Buttons.Y == ButtonState.Pressed)
+		{
+			//Invert active bool
+			Debug.Log("GAMEPAD characterIndex:" + gamepadData.characterIndex + ", gamepadPlayerIndex: " + gamepadData.gamepadPlayerIndex + ", active: " + gamepadData.active);
+			gamepadData.active = !gamepadData.active;
+			Debug.Log("After toggling active state, Gamepad active: "+gamepadData.active);
+			if (gamepadData.active)
+			{
+				// Playerdata newPlayerdata = new Playerdata();
+
+				// //Index should be first null
+				// for(int j = 0; j < gamepad.playerDataArray.Length; j++)
+				// {
+				// 	Debug.Log(gamepad.playerDataArray[j].characterIndex);
+				// 	if (gamepad.playerDataArray[j].characterIndex == -1)
+				// 	{
+				// 		Debug.Log("Activated gamepad's characterIndex: " + j);
+				// 		newPlayerdata.characterIndex = j;
+				// 		gamepadData.characterIndex = newPlayerdata.characterIndex;
+				// 		newPlayerdata.gamepadPlayerIndex = gamepadData.gamepadPlayerIndex;
+				// 		gamepad.playerDataArray[j] = newPlayerdata;
+				// 		Debug.Log("Added to array");
+				// 		break;
+				// 	}
+				// }
+					
+				// stateHandler.menuControl.AddPlayer(newPlayerdata.characterIndex);
+				
+			}
+			else
+			{
+				// bool removed = false;
+				// for (int j = 0; j < gamepad.playerDataArray.Length; j++)
+				// {
+				// 	if (!removed && gamepad.playerDataArray[j].gamepadPlayerIndex == gamepadData.gamepadPlayerIndex)
+				// 	{
+				// 		stateHandler.menuControl.RemovePlayer(gamepad.playerDataArray[j].characterIndex);
+				// 		removed = true;
+				// 		Debug.Log("deactivation of gamepad: " + gamepad.playerDataArray[j].gamepadPlayerIndex + ", characterIndex: " + gamepad.playerDataArray[j].characterIndex);
+				// 		gamepad.playerDataArray[j].characterIndex = -1;
+				// 	}
+				// }
+			}
+		}
+
+		// if (gamepadData.active && gamepadData.prevState.Buttons.A == ButtonState.Released && gamepadData.state.Buttons.A == ButtonState.Pressed)
+		// {
+		// 	stateHandler.menuControl.ToggleReady(gamepadData.characterIndex);
+		// }
+		// if (gamepadData.active && gamepadData.prevState.ThumbSticks.Left.X < 0.1f && gamepadData.state.ThumbSticks.Left.X > 0.1f)
+		// {
+		// 	if (stateHandler.menuControl.avatars)
+		// 		stateHandler.menuControl.avatars.ChangeAvatar(gamepadData.characterIndex, 1);
+		// }
+		// if (gamepadData.active && gamepadData.prevState.ThumbSticks.Left.X > -0.1f && gamepadData.state.ThumbSticks.Left.X < -0.1f)
+		// {
+		// 	if (stateHandler.menuControl.avatars)
+		// 		stateHandler.menuControl.avatars.ChangeAvatar(gamepadData.characterIndex, -1);
+		// }
+		return gamepadData;
+	}
+
+
+    public void ConnectToPlayers(List<GameObject> _players)
+    {
+        // for (int i = 0; i < _players.Count; i++)
+        // {
+        //     Debug.Log("connecting player " + _players[i].GetComponent<Player>().playerNumber);
+        //     Player temp = _players[i].GetComponent<Player>();
+        //     players.Add(temp);
+        // }       
+    }
+}
---
+++ SophisticatedRaccoons/Assets/Scripts/StateHandler.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public enum GameState
+{
+    menu = 0,
+    game = 1
+}
+
+public class StateHandler : MonoBehaviour
+{
+
+    #region Singleton
+    private static StateHandler _instance;
+    public static StateHandler Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                return null;
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+
+    public GameState gamestate = GameState.menu;
+    public GameObject playerPrefab;
+    public bool roundTest;
+
+    public delegate void onPlayersChosen();
+
+    // public MainMenuController menuControl;
+    public GamepadStateHandler gamepadStateHandler;
+    // public RoundManager roundManager;
+
+    private Mesh[] playerMeshes = new Mesh[4];
+    private MeshFilter[] playerMeshFilters = new MeshFilter[4];
+
+    // List<Playerdata> playersDatas = new List<Playerdata>();
+    List<GameObject> players = new List<GameObject>();
+
+    public List<GameObject> LeftPlayers()
+    {
+        List<GameObject> temp = new List<GameObject>();
+
+        foreach (GameObject p in players)
+        {
+            if (p.gameObject.activeSelf)
+            {
+                temp.Add(p);
+            }
+        }
+
+        return temp;
+    }
+
+
+
+    // Use this for initialization
+    void Awake()
+    {
+
+
+        if (GameObject.FindGameObjectsWithTag("GameMaster").Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        _instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.sceneLoaded += OnLevelLoaded;
+
+        Instantiate();
+
+    }
+
+    private void Update()
+    {
+        if (gamestate == GameState.game)
+        {
+            // if (LeftPlayers().Count < 2 || roundManager.TimeLeft() <= 0)
+            // {
+            //     roundManager.EndRound(LeftPlayers());
+            // }
+
+            // else if (roundManager.isOnRound)
+            // {
+            //     roundManager.UpdateRound();
+            // }
+
+        }
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelLoaded;
+
+    }
+
+    void OnLevelLoaded(Scene scene, LoadSceneMode mode)
+    {
+        //Gets called on sceneload
+        if (roundTest)
+        {
+            gamestate = scene.buildIndex != 0 ? GameState.game : GameState.menu;
+        }
+        else
+            gamestate = scene.buildIndex == 1 ? GameState.game : GameState.menu;
+        Instantiate();
+    }
+
+
+    void Instantiate()
+    {
+        if (!gamepadStateHandler)
+            gamepadStateHandler = GetComponent<GamepadStateHandler>();
+
+        // if (gamestate == GameState.menu)
+        // {
+        //     roundManager.enabled = false;
+        //     GameObject menu = GameObject.Find("Main Menu");
+        //     if (menu)
+        //         menuControl = menu.GetComponent<MainMenuController>();
+
+        // }
+        // else if (gamestate == GameState.game)
+        // {
+        //     SpawnPlayers();
+        //     AttachControllers();
+        //     roundManager.enabled = true;
+        //     roundManager.InitializeRounds(players.Count);
+        // }
+    }
+
+    private void SpawnPlayers()
+    {
+        int pDindex = 0;
+        foreach (var p in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            // if (pDindex < playersDatas.Count)
+            // {
+            //     if (p.GetComponent<Player>())
+            //     {
+
+            //         Player temp = p.GetComponentInParent<Player>();
+            //         temp.meshNumber = playersDatas[pDindex].meshNumber;
+            //         temp.playerNumber = playersDatas[pDindex].characterIndex;
+            //         if (!players.Contains(temp.gameObject))
+            //         {
+            //             players.Add(temp.gameObject);
+            //             pDindex++;
+
+            //         }
+            //         Debug.Log("Added player, meshNumber is:" + temp.meshNumber);
+            //     }
+            // }
+            // else
+            // {
+            //     p.SetActive(false);
+            // }
+
+        }
+
+        if (players.Count > 0)
+        {
+            foreach (GameObject player in players)
+            {
+                // var temp = player.GetComponentInChildren<Player>();
+                // if (!temp)
+                //     temp = player.GetComponentInParent<Player>();
+
+                // temp.skinnedMeshRenderer.sharedMesh = playerMeshes[temp.meshNumber];
+                // player.GetComponentInChildren<MeshFilter>().mesh = playerMeshes[player.GetComponent<Player>().meshNumber];
+
+            }
+        }
+    }
+
+    public void ResetAllPlayers()
+    {
+        foreach (GameObject p in players)
+        {
+            // p.GetComponent<Player>().TransportToStart();
+            p.SetActive(true);
+        }
+    }
+
+    private void AttachControllers()
+    {
+        GetComponent<InputHandler>().ConnectToPlayers(players);
+    }
+
+    // public void SavePlayerData(int amountJoined, Avatars avatars)
+    // {
+    //     // playerMeshes = menuControl.avatars.avatarMeshes;
+    //     // playerMeshFilters = menuControl.avatars.avatarMeshFilters;
+
+    //     // for (int i = 0; i < amountJoined; i++)
+    //     // {
+    //     //     Playerdata temp = new Playerdata();
+    //     //     temp.characterIndex = i;
+    //     //     temp.meshNumber = avatars.indexes[i];
+
+    //     //     playersDatas.Add(temp);
+    //     // }
+    // }
+
+    #region Getters
+
+    public int PlayerCount()
+    {
+        return players.Count;
+    }
+
+    // public int Winner()
+    // {
+    //     return roundManager.GetWinner() + 1;
+    // }
+
+    #endregion
+}
