24250d6fc09a20137ebd64eb9eaa7dbc8ee57325
Mitja <mitja.immonen@hotmail.com>
2018-06-26 15:07:26 +0300

    Player Camera transitions

--- SophisticatedRaccoons/Assets/Scripts/TacticsCamera.cs
+++ SophisticatedRaccoons/Assets/Scripts/TacticsCamera.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class TacticsCamera : MonoBehaviour {
 
+    float lerpTime = 2f;
+    bool rotationDone = false;
+
+    public void ChangePlayer(bool p1)
+    {
+        StartCoroutine(ChangeCamera(p1));
+    }
+
+    IEnumerator ChangeCamera(bool playerOne)
+    {
+        float time = Time.time;
+        float smoothLerp;
+        Vector3 oldAngle = transform.eulerAngles;
+        Vector3 newAngle = oldAngle;
+        newAngle.y = playerOne ? 0 : 180f;
+
+        while (time+lerpTime > Time.time)
+        {
+            float t = (Time.time-time)/lerpTime;
+            smoothLerp = t*t * (3f - 2f*t);
+            transform.eulerAngles = Vector3.Lerp(oldAngle, newAngle, smoothLerp);
+            yield return null;
+        }
+
+        transform.eulerAngles = newAngle;
+
+        yield break;
+    }
     public void RotateLeft()
     {
         transform.Rotate(Vector3.up, 90, Space.Self);
