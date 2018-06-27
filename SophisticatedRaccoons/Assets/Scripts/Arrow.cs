f05b649e03a269ccaffa3030ea5bb82307fc7b86
Unknown <antonio.omt@gmail.com>
2018-06-27 16:44:24 +0300

    turns but not actual turns + keyboard control

---
+++ SophisticatedRaccoons/Assets/Scripts/Arrow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Arrow : MonoBehaviour
+{
+    [StringInList("Up", "Down", "Right", "Left")] public string direction;
+    public bool selected = false;
+    Transform position;
+
+    void Awake()
+    {
+        
+    }
+    void LateUpdate()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (selected)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+
+    }
+
+    public void OnEnable()
+    {
+        selected = false;
+    }
+
+
+}
